2d37689f46e27025c66c174a5864b0fffc85be01
Coding-Huang <44698989+One-One-Code@users.noreply.github.com>
2018-11-23 18:30:45 +0800

    Update ZookeeperTest.cs

--- ZookeeperTest.cs
+++ ZookeeperTest.cs
@@ -57,66 +57,6 @@ namespace ConsoleTest
 
         }
 
-        private static void Fun()
-        {
-            Connect = false;
-            //创建一个Zookeeper实例，第一个参数为目标服务器地址和端口，第二个参数为Session超时时间，第三个为节点变化时的回调方法 
-            using (ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", new TimeSpan(0, 0, 0, 10), new Watcher()))
-            {
-                while (!Connect)
-                {
-
-                }
-
-                //var timeout = zk.SessionTimeout;
-                var stat = zk.Exists("/root", true);
-                if (stat == null)
-                {
-                    zk.Create("/root", null, Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
-                }
-                var watches = zk.DataWatches;
-                ////创建一个节点root，数据是mydata,不进行ACL权限控制，节点为永久性的(即客户端shutdown了也不会消失) 
-                //zk.Create("/root", "mydata".GetBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
-
-                //在root下面创建一个childone znode,数据为childone,不进行ACL权限控制，节点为永久性的 
-                if (zk.Exists("/root/childone", true) == null)
-                {
-                    var result = zk.Create("/root/childone", null, Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
-                }
-
-                //取得/root节点下的子节点名称,返回List<String> 
-                zk.GetChildren("/root", true);
-                //取得/root/childone节点下的数据,返回byte[] 
-                var data = zk.GetData("/root/childone", true, null);
-
-                //修改节点/root/childone下的数据，第三个参数为版本，如果是-1，那会无视被修改的数据版本，直接改掉
-                zk.SetData("/root/childone", "childonemodify".GetBytes(), -1);
-                //删除/root/childone这个节点，第二个参数为版本，－1的话直接删除，无视版本 
-                zk.Delete("/root/childone", -1);
-            }
-        }
-    }
-
-    /// <summary>
-    /// 注意Zookeeper的监听只能使用一次，原则上不允许永久监听，但可以在监听方法完成后再进行监听
-    /// </summary>
-    public class Watcher : IWatcher
-    {
-        public void Process(WatchedEvent @event)
-        {
-            //if (@event.Type == EventType.NodeDataChanged)
-            //{
-            //    Console.WriteLine("NodeDataChanged:" + @event.Path);
-            //}
-            if (KeeperState.SyncConnected == @event.State)
-            {
-                Console.WriteLine("已和服务建立连接");
-                ZooKeeperTest.Connect = true;
-            }
-            Console.WriteLine(@event.Type + ":" + @event.Path);
-        }
-    }
-
     /// <summary>
     /// 主体思路
     /// 1. 在locks节点下创建临时顺序节点node_n
